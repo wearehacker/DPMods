01c3b0d7a85b732f5feb6d9db8a3595eebaca8ad
wearehacker <modsgamer0@gmail.com>
2020-11-06 10:09:56 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+subscribe me on YouTube
+
+install
+first install git = pkg install git
+
+git clone <url>
+
+cd dpmods
+chmod +x *
+bash dpmods.sh
---
+++ dpmods.sh
@@ -0,0 +1,11 @@
+pkg install git -y 
+ pkg install python -y 
+ pkg install python2 -y 
+ termux-setup-storage 
+ pkg install figlet -y 
+ pkg install nano -y 
+ pkg install curl -y 
+pkg install php -y 
+pkg install lolcat -y
+figlet -f slant Thanks for Downloading | lolcat
+figlet -f slant SUBSCRIBE DPMods | lolcat
